189b86e487a0987d79f11173fa56a4634bdf8f1c
Tosin Afolabi <psyooa@nottingham.ac.uk>
2014-06-15 01:39:02 +0100

    initial commit & working api wrapper

--- PHManagerClient/PHViewController.m
+++ PHManagerClient/PHViewController.m
@@ -6,6 +6,7 @@
 //  Copyright (c) 2014 Tosin Afolabi. All rights reserved.
 //
 
+#import "PHManager.h"
 #import "PHViewController.h"
 
 @interface PHViewController ()
@@ -17,7 +18,45 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+
+    // Test getting today's products
+
+    [[PHManager sharedManager] getTodaysProductsWithSuccess:^(NSArray *products) {
+
+        NSLog(@"Products Download Successful");
+
+        for (PHProduct *product in products) {
+            NSLog(@"%@", [product titleWithTagline]);
+            NSLog(@"%@", [[product hunter] name]);
+        }
+
+        // Test getting comments for a product
+        PHProduct *topProduct = products[0];
+        [self getCommentsForProduct:topProduct];
+
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+
+        NSLog(@"The task: %@ failed with error: %@", task, error);
+
+    }];
+}
+
+- (void)getCommentsForProduct:(PHProduct *)product
+{
+
+    [[PHManager sharedManager] getCommentsForProduct:product success:^(NSArray *comments) {
+
+        NSLog(@"Comments Download Successful");
+
+        for (PHComment *comment in comments) {
+            NSLog(@"%@", [comment comment]);
+        }
+
+    } failure:^(NSURLSessionDataTask *task, NSError *error) {
+
+        NSLog(@"The task: %@ failed with error: %@", task, error);
+        
+    }];
 }
 
 - (void)didReceiveMemoryWarning
